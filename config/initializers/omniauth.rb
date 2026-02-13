056e07ac952474711d2372f951df90654e62adad
BahadurSinghRawatstartbit <bahadursinghrawat96@gmail.com>
2026-02-13 11:26:45 +0530

    do changes in onmiauth file write auth cred and access from env file

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,8 +4,10 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     config.logger=Rails.logger if Rails.env.development?
   end
   provider :google_oauth2,
-           Rails.application.credentials.dig(:google,:google_client_id),
-           Rails.application.credentials.dig(:google,:google_client_sk_id),
+          #  Rails.application.credentials.dig(:google,:google_client_id),
+          #  Rails.application.credentials.dig(:google,:google_client_sk_id),
+          ENV["GOOGLE_CLIENT_ID"],
+          ENV["GOOGLE_CLIENT_SECRET"],
            {
             include_granted_scopes:true,
             scope: 'email,profile,offline',
